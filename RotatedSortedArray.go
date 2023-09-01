e66e65a695e4d4ceae1a83ee4df5fe018ec7282b
vaibdix <dixitvaib@gmail.com>
2023-09-01 16:53:04 +0530

    rotatedArray updayed

--- RotatedSortedArray.go
+++ RotatedSortedArray.go
@@ -1,3 +1,31 @@
 func search(nums []int, target int) int {
+	var index, indexOfMax int
+	length := len(nums)
 
+	if length == 0 {
+		return -1
 	}
+
+	for indexOfMax+1 < length && nums[indexOfMax] < nums[indexOfMax+1] {
+		indexOfMax++
+	}
+
+	low, high, median := 0, length-1, 0
+	for low <= high {
+		median = (low + high) / 2
+		index = median + indexOfMax + 1
+		if index >= length {
+			index -= length
+		}
+		switch {
+		case nums[index] > target:
+			high = median - 1
+		case nums[index] < target:
+			low = median + 1
+		default:
+			return index
+		}
+	}
+
+	return -1
+}
