ccd5e3530399d83aa677bafe93ec7ccff3b43573
vaibdix <dixitvaib@gmail.com>
2023-09-01 16:47:22 +0530

    swapnode

---
+++ swapNodes.go
@@ -0,0 +1,11 @@
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	temp := head.Next
+	head.Next = swapPairs(temp.Next)
+	temp.Next = head
+
+	return temp
+}
