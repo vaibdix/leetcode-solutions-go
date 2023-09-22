7d6f833993fcdd498011ae39f306d3a82e51f985
vaibdix <dixitvaib@gmail.com>
2023-09-23 04:49:47 +0530

    roman to int

---
+++ romantointeger.go
@@ -0,0 +1,25 @@
+var RomanNumerals = map[rune]int{
+	'I': 1,
+	'V': 5,
+	'X': 10,
+	'L': 50,
+	'C': 100,
+	'D': 500,
+	'M': 1000,
+}
+
+func romanToInt(s string) int {
+	sum := 0
+	greatest := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		letter := s[i]
+		num := RomanNumerals[rune(letter)]
+		if num >= greatest {
+			greatest = num
+			sum = sum + num
+			continue
+		}
+		sum = sum - num
+	}
+	return sum
+}
